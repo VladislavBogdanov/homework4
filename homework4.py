bf8f4f15362af2c6f4c1ac4b4be4c9496b9f08f6
VladislavBogdanov <166297638+VladislavBogdanov@users.noreply.github.com>
2024-04-15 22:12:13 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+immutable_var=tuple= 23, 7, 1994, 'Дата рождения'
+print(immutable_var)
+#tuple[0]=25 # Кортеж это не изменяемый список!
+mutable_list= ["Шоколад", "сок", "киви"]
+print(mutable_list)
+mutable_list[0]= "кофе"
+mutable_list[2]= "чай"
+print(mutable_list)
